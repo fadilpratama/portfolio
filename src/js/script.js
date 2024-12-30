c2289966a82b8c89052c3b20d23d8c244ccf718a
MacBook Pro <macbookpro@192.168.1.5>
2024-12-30 23:17:25 +0700

    update content and button

--- src/js/script.js
+++ src/js/script.js
@@ -27,19 +27,19 @@ hamburger.addEventListener('click', function() {
 });
 
 // scroll to top
-const scrollToTopButton = document.querySelector('#scrollToTop');
+const scrollToTopButton = document.getElementById('scrollToTop');
 
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 300) {
-                scrollToTopButton.classList.remove('hidden');
-            } else {
-                scrollToTopButton.classList.add('hidden');
-            }
-        });
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            scrollToTopButton.classList.remove('hidden');
+        } else {
+            scrollToTopButton.classList.add('hidden');
+        }
+    });
 
-        scrollToTopButton.addEventListener('click', () => {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth',
-            });
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
         });
+    });
